45b94614912d6d880248b070a9368d5775694beb
Rauli Laine <rauli.laine@iki.fi>
2021-02-20 17:06:20 +0200

    Add namespace support to server
    Introduce support for namespaces in the server.

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -3,14 +3,9 @@ import express from 'express';
 import { Express } from 'express-serve-static-core';
 import morgan from 'morgan';
 
-import { Storage } from 'varasto-storage';
+import { InvalidSlugError, Storage } from '@varasto/storage';
 
-export interface ServerOptions {
-  auth?: {
-    username: string;
-    password: string;
-  };
-}
+import { ServerOptions } from './types';
 
 export const createServer = (
   storage: Storage,
@@ -21,7 +16,7 @@ export const createServer = (
   server.use(express.json());
   server.use(morgan('combined'));
 
-  if (options && options.auth) {
+  if (options?.auth) {
     const { username, password } = options.auth;
 
     server.use((req, res, next) => {
@@ -40,72 +35,91 @@ export const createServer = (
     });
   }
 
-  server.get('/:key', (req, res) => (
-    storage.getItem(req.params.key)
+  server.get('/:namespace/:key', (req, res) => {
+    const { namespace, key } = req.params;
+
+    storage.get(namespace, key)
       .then((value) => {
-        if (typeof value === 'undefined') {
-          res.status(404).json({
-            error: 'Item does not exist.',
-          });
+        if (value === undefined) {
+          res.status(404).json({ error: 'Item does not exist.' });
         } else {
           res.status(200).json(value);
         }
       })
-      .catch(() => res.status(500).json({
-        error: 'Unable to retrieve item.',
-      }))
-  ));
+      .catch((err) => {
+        if (err instanceof InvalidSlugError) {
+          res.status(400).json({ error: err.message });
+        } else {
+          res.status(500).json({ error: 'Unable to retrieve item.' });
+        }
+      });
+  });
 
-  server.post('/:key', (req, res) => (
-    storage.setItem(req.params.key, req.body)
+  server.post('/:namespace/:key', (req, res) => {
+    const { namespace, key } = req.params;
+
+    storage.set(namespace, key, req.body)
       .then(() => res.status(201).json(req.body))
-      .catch(() => res.status(500).json({
-        error: 'Unable to store item.',
-      }))
-  ));
+      .catch((err) => {
+        if (err instanceof InvalidSlugError) {
+          res.status(400).json({ error: err.message });
+        } else {
+          res.status(500).json({ error: 'Unable to store item.' });
+        }
+      });
+  });
 
-  server.patch('/:key', (req, res) => (
-    storage.getItem(req.params.key)
+  server.patch('/:namespace/:key', (req, res) => {
+    const { namespace, key } = req.params;
+
+    storage.get(namespace, key)
       .then((value) => {
-        if (typeof value === 'undefined') {
-          res.status(404).json({
-            error: 'Item does not exist.',
-          });
+        if (value === undefined) {
+          res.status(404).json({ error: 'Item does not exist.' });
           return;
         }
 
-        const result = {...value, ...req.body};
+        const result = { ...value, ...req.body };
 
-        storage.setItem(req.params.key, result)
+        storage.set(namespace, key, result)
           .then(() => res.status(201).json(result))
           .catch(() => res.status(500).json({
             error: 'Unable to store item.',
           }));
       })
-      .catch(() => res.status(500).json({
-        error: 'Unable to retrieve item.',
-      }))
-  ));
-
-  server.delete('/:key', (req, res) => (
-    storage.getItem(req.params.key)
-      .then((value) => {
-        if (typeof value === 'undefined') {
-          res.status(404).json({
-            error: 'Item does not exist.',
-          });
+      .catch((err) => {
+        if (err instanceof InvalidSlugError) {
+          res.status(400).json({ error: err.message });
         } else {
-          storage.removeItem(req.params.key)
-            .then(() => res.status(201).json(value))
-            .catch(() => res.status(500).json({
-              error: 'Unable to remove item.',
-            }));
+          res.status(500).json({ error: 'Unable to retrieve item.' });
         }
+      });
+  });
+
+  server.delete('/:namespace/:key', (req, res) => {
+    const { namespace, key } = req.params;
+
+    storage.get(namespace, key)
+      .then((value) => {
+        if (value === undefined) {
+          res.status(404).json({ error: 'Item does not exist.' });
+          return;
+        }
+
+        storage.delete(namespace, key)
+          .then(() => res.status(201).json(value))
+          .catch(() => res.status(500).json({
+            error: 'Unable to remove item.',
+          }));
       })
-      .catch(() => res.status(500).json({
-        error: 'Unable to remove item.',
-      }))
-  ));
+      .catch((err) => {
+        if (err instanceof InvalidSlugError) {
+          res.status(400).json({ error: err.message });
+        } else {
+          res.status(500).json({ error: 'Unable to remove item.' });
+        }
+      });
+  });
 
   return server;
 };
