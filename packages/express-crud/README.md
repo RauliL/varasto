d9b33c055ea07e859d27341feb47da897b74f84d
Rauli Laine <rauli.laine@iki.fi>
2022-01-08 11:02:23 +0200

    express-crud v3.0.0
    Couple of changes on how `express-crud` behaves. These include:
    
    - Keys for new entries are now automatically generated. You can either
      provide your own custom key generator or UUIDs.
    - Validation schema is now optional.
    - Router factory function (and it's configuration) are now typed.

--- packages/express-crud/README.md
+++ packages/express-crud/README.md
@@ -1,12 +1,19 @@
 # @varasto/express-crud
 
 Utility for generating [Express.js] [router] that handles [CRUD] operations for
-a single namespace in Varasto storage, with validation performed by [Yup].
+a single namespace in Varasto storage, with optional validation performed by
+[Yup].
+
+It is similar to [Varasto server] (which can also be used as [Express.js]
+[router]) expect that it only handles single namespace in the storage,
+generates automatically keys for new items stored in the namespace and provides
+optional validation.
 
 [express.js]: https://expressjs.com/
 [router]: https://expressjs.com/en/4x/api.html#router
 [crud]: https://en.wikipedia.org/wiki/Create,_read,_update_and_delete
 [yup]: https://github.com/jquense/yup
+[varasto server]: https://www.npmjs.com/package/@varasto/server
 
 ## Installation
 
@@ -18,7 +25,7 @@ $ npm install --save @varasto/express-crud
 
 The package provides an function called `createRouter`, which creates an
 Express.js [router] that provides basic [CRUD] operations to an namespace
-in Varasto storage, with validation provided by [Yup].
+in Varasto storage, with optional validation provided by [Yup].
 
 ```TypeScript
 import { createRouter } from '@varasto/express-crud';
@@ -28,7 +35,7 @@ import * as yup from 'yup';
 
 const storage = createFileSystemStorage({ dir: './data' });
 
-const peopleSchema = yup.object().shape({
+const personSchema = yup.object().shape({
   name: yup.string().required(),
   age: yup.number().required().positive().integer(),
   email: yup.string().email(),
@@ -38,11 +45,160 @@ const app = express();
 
 express.use(
   '/api/people',
-  createRouter(storage, 'people', peopleSchema)
+  createRouter(storage, 'people', { schema: personSchema })
 );
 ```
 
 The Express.js application will now respond to `GET`, `POST`, `PATCH` and
 `DELETE` requests made into URI `/api/people`. Data passed with `POST` and
-`PATCH` requests must pass schema validation. All data will be stored under
-namespace `people`.
+`PATCH` requests must pass schema validation, if one is provided. All data
+will be stored under namespace `people`.
+
+### Options
+
+The function takes an optional configuration object, which supports these
+settings:
+
+#### `schema`
+
+Optional [Yup] schema to which all data is validated against. If omitted, no
+validation is performed at all.
+
+#### `keyGenerator`
+
+Optional function that will be used to generate keys for new entries that are
+created with `POST` requests. If omitted, [UUID] keys will be generated for all
+new entries intead.
+
+[uuid]: https://en.wikipedia.org/wiki/Universally_unique_identifier
+
+### Listing items
+
+To list all items stored under the namespace, you make an `GET` request without
+any key included in the request path.
+
+```http
+GET / HTTP/1.0
+```
+
+To which the router will respond with an JSON object which contains each item
+stored under the namespace mapped with the key that is used to identify each
+individual item.
+
+```JSON
+{
+  "b24ed42e-6ffd-11ec-a909-97c71e1c1df5": {
+    "name": "John Doe",
+    "age": 25,
+    "email": "john.doe@example.com"
+  },
+  "bc846d00-6ffd-11ec-972a-a7b216391618": {
+    "name": "Jane Doe",
+    "age": 30,
+    "email": "jane.doe@example.com"
+  }
+}
+```
+
+### Creating new items
+
+To create new item under the namespace, you make an `POST` request without any
+key included in the request path, with value of the new item included in the
+request body as JSON.
+
+```http
+POST / HTTP/1.0
+Content-Type: application/json
+Content-Length: 72
+
+{
+  "name": "John Doe",
+  "age": 25,
+  "email": "john.doe@example.com"
+}
+```
+
+The router will assign an key to the newly created item which will be used to
+identify the item with. Key of the item will be returned as the response.
+
+This key can be later used to identify the created item.
+
+```JSON
+{
+  "key": "403ecc3e-6fff-11ec-87d5-bf4f846a6b19"
+}
+```
+
+### Replacing existing items
+
+To replace an already existing item, you make an `POST` request with key of the
+item you wish to replace as part of the request path, with new value of the
+item included in the request body as JSON.
+
+```http
+POST /403ecc3e-6fff-11ec-87d5-bf4f846a6b19 HTTP/1.0
+Content-Type: application/json
+Content-Length: 72
+
+{
+  "name": "John Doe",
+  "age": 26,
+  "email": "john.doe@example.com"
+}
+```
+
+If the item does not exist in the namespace, 404 will be returned as response
+instead.
+
+If optional schema was given to the router, an validation will be performed
+against it. If the given data does not pass the validation 404 error will be
+returned as response, with the response body containing details on why the
+validation failed.
+
+### Updating existing items
+
+To partially update an already existing item, you make an `PATCH` request with
+key of the item you wish to update as part of the request path. The JSON
+included in response body will be shallowly merged with the already existing
+data and the result will be sent as response.
+
+```http
+PATCH /403ecc3e-6fff-11ec-87d5-bf4f846a6b19 HTTP/1.0
+Content-Type: application/json
+Content-Length: 15
+
+{
+  "age": 26
+}
+```
+
+And the server will respond with:
+
+```JSON
+{
+  "name": "John Doe",
+  "age": 26,
+  "email": "john.doe@example.com"
+}
+```
+
+If the item does not exist in the namespace, 404 will be returned as response
+instead.
+
+If optional schema was given to the router, an validation will be performed
+against it. If the updated data does not pass the validation 404 error will be
+returned as response, with the response body containing details on why the
+validation failed.
+
+### Removing items
+
+To remove an item, you make a `DELETE` request with key of the item you wish to
+delete as part of the request path.
+
+```http
+DELETE /403ecc3e-6fff-11ec-87d5-bf4f846a6b19 HTTP/1.0
+```
+
+If the item exists in the namespace, it will be removed and it's value is
+returned as response. If the item does not exist, 404 error will be returned
+as response instead.
--- packages/express-crud/src/router.ts
+++ packages/express-crud/src/router.ts
@@ -1,8 +1,11 @@
 import { InvalidSlugError, Storage } from '@varasto/storage';
 
-import express, { Request, Response, Router } from 'express';
+import express, { Response, Router } from 'express';
+import { v4 as uuid } from 'uuid';
 import { JsonObject } from 'type-fest';
-import { AnySchema, ValidationError } from 'yup';
+import { ValidationError } from 'yup';
+
+import { RouterOptions } from './types';
 
 /**
  * Constructs an router that provides CRUD operations for an namespace in given
@@ -11,65 +14,76 @@ import { AnySchema, ValidationError } from 'yup';
  *
  * @param storage Varasto storage to used for storing the data.
  * @param namespace Namespace where the data will be stored into.
- * @param schema An Yup schema which will be used to validate all incoming
- *               data.
+ * @param options Various options that can be given to the router.
  * @returns An Express.js router that provides CRUD operations which work on
  *          data stored in specified namespace in the Varasto storage.
  */
-export const createRouter = (
+export const createRouter = <T extends JsonObject = JsonObject>(
   storage: Storage,
   namespace: string,
-  schema: AnySchema
+  options: Partial<RouterOptions<T>> = {}
 ): Router => {
   const router = express();
+  const schema = options.schema;
+  const keyGenerator = options.keyGenerator ?? uuid;
 
-  const retrieve =
-    (callback: (req: Request, res: Response, value: JsonObject) => void) =>
-    (req: Request, res: Response) => {
+  const validate = (
+    res: Response,
+    data: T,
+    onSuccess: (validData: T) => void
+  ) => {
+    if (schema) {
+      schema
+        .validate(data, {
+          strict: false,
+          stripUnknown: true,
+        })
+        .then(onSuccess)
+        .catch((err: ValidationError) => {
+          res.status(400).json({
+            error: 'Data did not pass validation.',
+            errors: err.errors,
+          });
+        });
+    } else {
+      onSuccess(data);
+    }
+  };
+
+  const get = (res: Response, key: string, onSuccess: (value: T) => void) => {
+    storage
+      .get<T>(namespace, key)
+      .then((value) => {
+        if (value === undefined) {
+          res.status(404).json({ error: 'Item does not exist.' });
+        } else {
+          onSuccess(value);
+        }
+      })
+      .catch((err) => {
+        if (err instanceof InvalidSlugError) {
+          res.status(400).json({ error: err.message });
+        } else {
+          res.status(500).json({ error: 'Unable to retrieve item.' });
+        }
+      });
+  };
+
+  const store = (res: Response, key: string, data: T) => {
+    validate(res, data, (validData) => {
       storage
-        .get(namespace, req.params.key)
-        .then((value) => {
-          if (value === undefined) {
-            res.status(404).json({ error: 'Item does not exist.' });
-          } else {
-            callback(req, res, value as JsonObject);
-          }
+        .set<T>(namespace, key, validData)
+        .then(() => {
+          res.status(201).json(validData);
         })
         .catch((err) => {
           if (err instanceof InvalidSlugError) {
             res.status(400).json({ error: err.message });
           } else {
-            res.status(500).json({ error: 'Unable to retrieve item.' });
+            res.status(500).json({ error: 'Unable to store item.' });
           }
         });
-    };
-
-  const store = (req: Request, res: Response, value: JsonObject) => {
-    schema
-      .validate(value, {
-        strict: false,
-        stripUnknown: true,
-      })
-      .then((validValue) => {
-        storage
-          .set(namespace, req.params.key, validValue)
-          .then(() => {
-            res.status(201).json(validValue);
-          })
-          .catch((err) => {
-            if (err instanceof InvalidSlugError) {
-              res.status(400).json({ error: err.message });
-            } else {
-              res.status(500).json({ error: 'Unable to store item.' });
-            }
-          });
-      })
-      .catch((err: ValidationError) => {
-        res.status(400).json({
-          error: 'Data did not pass validation.',
-          errors: err.errors,
-        });
-      });
+    });
   };
 
   router.use(express.json());
@@ -97,37 +111,65 @@ export const createRouter = (
       });
   });
 
-  router.get(
-    '/:key',
-    retrieve((req, res, value) => {
+  router.get('/:key', (req, res) => {
+    get(res, req.params.key, (value) => {
       res.status(200).json(value);
-    })
-  );
-
-  router.post('/:key', (req, res) => {
-    store(req, res, req.body);
+    });
   });
 
-  router.patch(
-    '/:key',
-    retrieve((req, res, value) => {
-      store(req, res, { ...value, ...req.body });
-    })
-  );
+  router.post('/', (req, res) => {
+    validate(res, req.body, (validData) => {
+      const key = keyGenerator(validData);
 
-  router.delete(
-    '/:key',
-    retrieve((req, res, value) => {
+      storage
+        .set<T>(namespace, key, validData)
+        .then(() => {
+          res.status(201).json({ key });
+        })
+        .catch((err) => {
+          if (err instanceof InvalidSlugError) {
+            res.status(400).json({ error: err.message });
+          } else {
+            res.status(500).json({ error: 'Unable to store item.' });
+          }
+        });
+    });
+  });
+
+  router.post('/:key', (req, res) => {
+    const { key } = req.params;
+
+    get(res, key, () => {
+      store(res, key, req.body);
+    });
+  });
+
+  router.patch('/:key', (req, res) => {
+    const { key } = req.params;
+
+    get(res, key, (oldData) => {
+      store(res, key, { ...oldData, ...req.body });
+    });
+  });
+
+  router.delete('/:key', (req, res) => {
+    const { key } = req.params;
+
+    get(res, key, (value) => {
       storage
         .delete(namespace, req.params.key)
         .then(() => {
           res.status(201).json(value);
         })
-        .catch(() => {
-          res.status(500).json({ error: 'Unable to remove item.' });
+        .catch((err) => {
+          if (err instanceof InvalidSlugError) {
+            res.status(400).json({ error: err.message });
+          } else {
+            res.status(500).json({ error: 'Unable to remove item.' });
+          }
         });
-    })
-  );
+    });
+  });
 
   return router;
 };
