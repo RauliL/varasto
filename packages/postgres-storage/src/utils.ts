45ac2d7a32c80876195aa8e0d2a334d77db57028
Rauli Laine <rauli.laine@iki.fi>
2023-09-30 14:19:34 +0300

    Update @varasto/postgres-storage

--- packages/postgres-storage/src/utils.ts
+++ packages/postgres-storage/src/utils.ts
@@ -1,6 +1,7 @@
 import { InvalidSlugError } from '@varasto/storage';
 import { isValidSlug } from 'is-valid-slug';
 import { Client } from 'pg';
+import format from 'pg-format';
 import { JsonObject } from 'type-fest';
 
 export const validateNamespace = (namespace: string) => {
@@ -22,24 +23,32 @@ export const doesNamespaceExist = (
 ): Promise<boolean> =>
   client
     .query(
-      `
+      format(
+        `
     SELECT EXISTS (
       SELECT FROM information_schema.tables
       WHERE table_schema = 'public'
-      AND table_name = '${namespace}'
+      AND table_name = %I
     );
-    `
+    `,
+        namespace
+      )
     )
     .then((result) => result.rows?.[0]?.exists === true);
 
 export const createNamespace = (client: Client, namespace: string) =>
-  client.query(`
-    CREATE TABLE IF NOT EXISTS "${namespace}" (
+  client.query(
+    format(
+      `
+    CREATE TABLE IF NOT EXISTS %I (
       key TEXT PRIMARY KEY NOT NULL,
       value JSONB NOT NULL,
       UNIQUE (key)
     )
-  `);
+  `,
+      namespace
+    )
+  );
 
 export const hasItem = async (
   client: Client,
@@ -50,8 +59,7 @@ export const hasItem = async (
 
   if (await doesNamespaceExist(client, namespace)) {
     const result = await client.query(
-      `SELECT COUNT(*) FROM "${namespace}" WHERE key = $1`,
-      [key]
+      format('SELECT COUNT(*) FROM %I WHERE key = %L', namespace, key)
     );
 
     return result.rows?.[0]?.count > 0;
@@ -69,8 +77,7 @@ export const getItem = async <T extends JsonObject>(
 
   if (await doesNamespaceExist(client, namespace)) {
     const result = await client.query(
-      `SELECT value FROM "${namespace}" WHERE key = $1`,
-      [key]
+      format('SELECT value FROM %I WHERE key = %L', namespace, key)
     );
 
     if (result.rows.length > 0) {
