3d8e0bab1628e5a134a099d8a1e618dae6aa9fc4
Rauli Laine <rauli.laine@iki.fi>
2026-01-07 13:07:18 +0200

    Ignore some files in test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
+      exclude: ['src/index.ts'],
+      include: ['src/**/*.ts'],
       provider: 'v8',
       reporter: ['lcov', 'text'],
     },
